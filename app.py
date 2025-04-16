f83dbe994d8485a1a372af2a25f5f3e4e2f7d12f
wexist2003 <146671844+wexist2003@users.noreply.github.com>
2025-04-16 17:30:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,7 @@ def get_user_code(user_id):
 # ──────── ЗАПУСК ────────
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
