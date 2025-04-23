bf47f3e9d7803605e13961a5f9a59689441cb1d0
wexist2003 <146671844+wexist2003@users.noreply.github.com>
2025-04-23 15:58:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -401,4 +401,4 @@ def place_card(code, image_id):
 if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)  # Убедитесь, что debug=True
